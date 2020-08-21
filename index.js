31be72613048c2f0c057fb32fd1c27ba58c88ece
Muhammad Mahbubur Rahman <muhammadmahbuburrahman@Muhammads-MacBook-Air.local>
2020-08-21 23:12:48 +0900

    basics end

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-function inchToMeter(inch)
-var meter = 
+//js basics
+
+function inchToMeter(inch){
+var meter = inch * 0.0254;
+return meter;
+}
+
+var new3 = inchToMeter(8999);
+console.log(new3);
+
+var friends = [12,89,56,78]
+console.log(friends[2])
+friends[3]= 45;
+
+friends.push(100)
+
+friends.pop()
+console.log(friends)
+friends.shift()
+console.log(friends)
